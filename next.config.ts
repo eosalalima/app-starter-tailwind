33bc30fbd7701a9518441259b90822a248804e84
eric_salalima <eosalalima@gmail.com>
2025-12-30 20:37:31 +0800

    Setup prisma

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        domains: [
+            "plus.unsplash.com",
+            "images.unsplash.com",
+            "res.cloudinary.com",
+            "cdn.pixabay.com",
+            "images.pexels.com",
+            "tailwindcss.com",
+            "randomuser.me",
+        ],
+    },
 };
 
 export default nextConfig;
